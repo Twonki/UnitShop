03b65e3b6465842ecdd7e74a3ea01d5c993611eb
Twonki <Leonhard.Applis@Protonmail.com>
2019-09-23 21:13:39 +0200

    whoopsiedaisy

--- Game/Round2_Ant/src/Code/SimpleFood.java
+++ Game/Round2_Ant/src/Code/SimpleFood.java
@@ -4,55 +4,55 @@ import java.awt.Point;
 
 public class SimpleFood implements Food {
 
+
 	public String name;
-	private Point point = new Point();
+	private Point point;
 	public int nutritionValue =  0;
 	
 	public SimpleFood(String name, double x, double y, int nutritionValue) {
-	if(name != null && name != "")	
-		this.name = name;
-	else 
-		throw new IllegalArgumentException("Name is null or empty");
-	if(x >= 0 && y >= 0)
-		this.point.setLocation(x, y);
-	else 
-		throw new IllegalArgumentException("x or y value of the point is smaler then zero");
-	if(nutritionValue >= 0)
-		this.nutritionValue = nutritionValue;
-	else 
-		throw new IllegalArgumentException("nutrition value is smaler then zero");
+		if(name != null && name != "")	
+			this.name = name;
+		else 
+			throw new IllegalArgumentException("Name cannot be null or empty!");
+		if(x >= 0 && y >= 0)
+			this.point.setLocation(x, y);
+		else 
+			throw new IllegalArgumentException("Location must consist of positive numbers!");
+		if(nutritionValue >= 0)
+			this.nutritionValue = nutritionValue;
+		else 
+			throw new IllegalArgumentException("Nutrition Value must be positive");	
 	}
 	
 	@Override
 	public boolean equals(Object o) {
-		if(o instanceof SimpleFood)
-		{
-			var foodobject = (SimpleFood) o;
-			return (this.name == foodobject.name && this.point.x == foodobject.point.x && this.point.y == foodobject.point.y && this.nutritionValue == foodobject.nutritionValue);	
-		}
-		else
+		if (o == this)
+			return true;
+		if (! (o instanceof Food))
 			return false;
+
+		Food foodobject = (Food) o;
+		return 
+			this.name == foodobject.name 
+			&& this.point.x == foodobject.point.x 
+			&& this.point.y == foodobject.point.y 
+			&& this.nutritionValue == foodobject.nutritionValue;	
 	}
 	
-	@Override
 	public int nibble(int nibble) {
-	if(this.nutritionValue >= nibble) {
-		this.nutritionValue = this.nutritionValue - nibble;
-		return nibble;
+		if (this.nutritionValue >= nibble) {
+			this.nutritionValue = this.nutritionValue - nibble;
+			return nibble;
+		} else {
+			this.nutritionValue = 0;
+			return nutritionValue;
+		}
 	}
-	else
-	{
-		this.nutritionValue = 0;
-		return nutritionValue;
-	}
-	}
-
-	@Override
+	
 	public Point getPoint() {
 		return this.point;
 	}
 
-	@Override
 	public void setPoint(Point point) {
 		this.point = point;
 	}
