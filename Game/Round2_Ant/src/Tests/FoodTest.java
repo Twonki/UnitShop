8a81ca9891429f4a0b97fd6d59aace963f9f7d62
Twonki <Leonhard.Applis@Protonmail.com>
2019-09-23 21:15:42 +0200

    altered foodtests to work - hopefully

--- Game/Round2_Ant/src/Tests/FoodTest.java
+++ Game/Round2_Ant/src/Tests/FoodTest.java
@@ -4,13 +4,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.awt.Point;
 import org.junit.jupiter.api.Test;
 
-import Code.Food;
+import Code.SimpleFood;
 
 public class FoodTest {
 	
 	@Test
 	void testConstructor_allValuesAreOk_shouldBeBuildWithSetValues() {
-		Food testobject = new Food("testfood",1,1,50);
+		SimpleFood testobject = new SimpleFood("testfood",1,1,50);
 		assertTrue(testobject.name == "testfood");
 		assertTrue(testobject.getPoint().x == 1);
 		assertTrue(testobject.getPoint().y == 1);
@@ -19,103 +19,103 @@ public class FoodTest {
 	
 	@Test
 	void testConstructor_nameIsNull_shouldThrowException() {
-		assertThrows(IllegalArgumentException.class, () -> new Food(null,1,1,100));
+		assertThrows(IllegalArgumentException.class, () -> new SimpleFood(null,1,1,100));
 	}
 	
 	@Test
 	void testConstructor_nameIsEmpty_shouldThrowException() {
-		assertThrows(IllegalArgumentException.class, () -> new Food("",1,1,100));
+		assertThrows(IllegalArgumentException.class, () -> new SimpleFood("",1,1,100));
 	}
 	
 	@Test
 	void testConstructor_xPointIsNegative_shouldThrowException() {
-		assertThrows(IllegalArgumentException.class, () -> new Food("test",-1,1,100));
+		assertThrows(IllegalArgumentException.class, () -> new SimpleFood("test",-1,1,100));
 	}
 	
 	@Test
 	void testConstructor_yPointIsNegative_shouldThrowException() {
-		assertThrows(IllegalArgumentException.class, () -> new Food("test",1,-1,100));
+		assertThrows(IllegalArgumentException.class, () -> new SimpleFood("test",1,-1,100));
 	}
 	
 	@Test
 	void testConstructor_nutritionValueIsNegative_shouldThrowException() {
-			assertThrows(IllegalArgumentException.class, () -> new Food("test",1,1,-10));
+			assertThrows(IllegalArgumentException.class, () -> new SimpleFood("test",1,1,-10));
 	}
 	
 	@Test
 	void testEquals_sameObject_shouldBeEqualToItself_shouldBeTrue() {
-		Food testobject = new Food("testfood",1,1,1);
+		SimpleFood testobject = new SimpleFood("testfood",1,1,1);
 
 		assertTrue(testobject.equals(testobject));
 	}
 
 	@Test
 	void testEquals_sameObjects_shouldBeEqual_shouldBeTrue() {
-		Food testobject1 = new Food("testfood",1,1,1);
-		Food testobject2 = new Food("testfood",1,1,1);
+		SimpleFood testobject1 = new SimpleFood("testfood",1,1,1);
+		SimpleFood testobject2 = new SimpleFood("testfood",1,1,1);
 
 		assertTrue(testobject1.equals(testobject2));
 	}
 	
 	@Test
 	void testEquals_sameNameDifferentPositionandNutritionvalue_shouldBeFalse() {
-		Food testobject1 = new Food("testfood",2,2,2);
-		Food testobject2 = new Food("testfood",1,1,1);
+		SimpleFood testobject1 = new SimpleFood("testfood",2,2,2);
+		SimpleFood testobject2 = new SimpleFood("testfood",1,1,1);
 
 		assertFalse(testobject1.equals(testobject2));
 	}
 	
 	@Test
 	void testEquals_sameNameAndPositionDifferentNutritionvalue_shouldBeFalse() {
-		Food testobject1 = new Food("testfood",1,1,2);
-		Food testobject2 = new Food("testfood",1,1,1);
+		SimpleFood testobject1 = new SimpleFood("testfood",1,1,2);
+		SimpleFood testobject2 = new SimpleFood("testfood",1,1,1);
 
 		assertFalse(testobject1.equals(testobject2));
 	}
 	
 	@Test
 	void testEquals_sameNameAndNutritionvalueDifferentPosition_shouldBeFalse() {
-		Food testobject1 = new Food("testfood",2,2,1);
-		Food testobject2 = new Food("testfood",1,1,1);
+		SimpleFood testobject1 = new SimpleFood("testfood",2,2,1);
+		SimpleFood testobject2 = new SimpleFood("testfood",1,1,1);
 
 		assertFalse(testobject1.equals(testobject2));
 	}
 	
 	@Test
 	void testEquals_samePositionDifferentNameAndNutritionvalue_shouldBeFalse() {
-		Food testobject1 = new Food("testfood",1,1,2);
-		Food testobject2 = new Food("testfood1",1,1,1);
+		SimpleFood testobject1 = new SimpleFood("testfood",1,1,2);
+		SimpleFood testobject2 = new SimpleFood("testfood1",1,1,1);
 
 		assertFalse(testobject1.equals(testobject2));
 	}
 	
 	@Test
 	void testEquals_samePositionAndNutritionvalueDifferentName_shouldBeFalse() {
-		Food testobject1 = new Food("testfood",1,1,1);
-		Food testobject2 = new Food("testfood1",1,1,1);
+		SimpleFood testobject1 = new SimpleFood("testfood",1,1,1);
+		SimpleFood testobject2 = new SimpleFood("testfood1",1,1,1);
 
 		assertFalse(testobject1.equals(testobject2));
 	}
 	
 	@Test
 	void testEquals_sameNutritionValueDifferentPositionAndName_shouldBeFalse() {
-		Food testobject1 = new Food("testfood",1,1,1);
-		Food testobject2 = new Food("testfood1",2,2,1);
+		SimpleFood testobject1 = new SimpleFood("testfood",1,1,1);
+		SimpleFood testobject2 = new SimpleFood("testfood1",2,2,1);
 
 		assertFalse(testobject1.equals(testobject2));
 	}
 	
 	@Test
 	void testEquals_DifferentNameAndPositionAndNutritionValue_shouldBeFalse() {
-		Food testobject1 = new Food("testfood",2,2,2);
-		Food testobject2 = new Food("testfood1",1,1,1);
+		SimpleFood testobject1 = new SimpleFood("testfood",2,2,2);
+		SimpleFood testobject2 = new SimpleFood("testfood1",1,1,1);
 
 		assertFalse(testobject1.equals(testobject2));
 	}
 	
 	@Test
 	void testEquals_compareToObjectOfDifferentClass_shouldBeFalse_shouldBeFalse() {
-		Food testobject1 = new Food("testfood",2,2,2);
+		SimpleFood testobject1 = new SimpleFood("testfood",2,2,2);
 		Point testobject2 = new Point(1,1);
 
 		assertFalse(testobject1.equals(testobject2));
@@ -125,7 +125,7 @@ public class FoodTest {
 	//How to make this dynamic
 	@Test
 	void testNibble_inputIsNumberSmallerThanNutrition_shouldReturnFullNibbleAmount() {
-		Food testobject = new Food("doesntmatter",0,0,50);
+		SimpleFood testobject = new SimpleFood("doesntmatter",0,0,50);
 		
 		int result = testobject.nibble(10);
 		
@@ -133,7 +133,7 @@ public class FoodTest {
 	}
 	
 	void testNibble_inputIsSmallerThanNutritionValue_NutritionValueShouldBeReducedByNibbleAMount() {
-		Food testobject = new Food("doesntmatter",0,0,50);
+		SimpleFood testobject = new SimpleFood("doesntmatter",0,0,50);
 		
 		testobject.nibble(10);
 		
@@ -142,7 +142,7 @@ public class FoodTest {
 	
 	@Test
 	void testNibble_inputIsNumberBiggerThanNutrition_shouldReturnNutritionEqualsZero() {
-		Food testobject = new Food("testfooo",1,1,50);
+		SimpleFood testobject = new SimpleFood("testfooo",1,1,50);
 		
 		testobject.nibble(60);
 		
@@ -152,7 +152,7 @@ public class FoodTest {
 	@Test
 	void testGetPoint_returnPoint_shouldReturnFoodObjectPoint()
 	{
-		Food testobject = new Food("testfooo",1,1,50);
+		SimpleFood testobject = new SimpleFood("testfooo",1,1,50);
 		Point testpoint = new Point(1,1);
 		
 		assertTrue(testpoint.x == testobject.getPoint().getX() && testpoint.y == testobject.getPoint().y);
@@ -160,7 +160,7 @@ public class FoodTest {
 	
 	@Test
 	void testSetPoint_setPointToValue_pointShouldGetSetValues()	{
-		Food testobject = new Food("testfooo",1,1,50);
+		SimpleFood testobject = new SimpleFood("testfooo",1,1,50);
 		Point testpoint = new Point(2,2);
 		testobject.setPoint(testpoint);
 		
