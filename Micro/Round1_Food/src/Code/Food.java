075dd8ead42e1904b443fb317ed71b80c700427e
Twonki <Leonhard.Applis@Protonmail.com>
2019-12-15 17:03:27 +0100

    Cleaned MicroFood for Round 1

--- Micro/Round1_Food/src/Code/Food.java
+++ Micro/Round1_Food/src/Code/Food.java
@@ -6,49 +6,19 @@ public class Food {
 
 	public String name;
 	private Point point;
-	public int nutritionValue =  0;
+	public int nutritionValue;
 	
 	public Food(String name, double x, double y, int nutritionValue) {
-		if(name != null && name != "")	
-			this.name = name;
-		else 
-			throw new IllegalArgumentException("Name cannot be null or empty!");
-		if(x >= 0 && y >= 0) {
-			point = new Point();
-			point.setLocation(x, y);}
-		else 
-			throw new IllegalArgumentException("Location must consist of positive numbers!");
-		if(nutritionValue >= 0)
-			this.nutritionValue = nutritionValue;
-		else 
-			throw new IllegalArgumentException("Nutrition Value must be positive");	
+		throw new UnsupportedOperationException("Not yet implemented");	
 	}
 	
 	@Override
 	public boolean equals(Object o) {
-		if (o == this)
-			return true;
-		if (! (o instanceof Food))
-			return false;
-
-		Food foodobject = (Food) o;
-		return 
-			this.name == foodobject.name 
-			&& this.point.x == foodobject.point.x 
-			&& this.point.y == foodobject.point.y 
-			&& this.nutritionValue == foodobject.nutritionValue;	
+		throw new UnsupportedOperationException("Not yet implemented");
 	}
 	
 	public int nibble(int nibble) {
-		if (this.nutritionValue >= nibble) {
-			this.nutritionValue = this.nutritionValue - nibble;
-			return nibble;
-		} else {
-			int nibbledAmount = nutritionValue;
-			this.nutritionValue = 0;
-			return nibbledAmount;
-
-		}
+		throw new UnsupportedOperationException("Not yet implemented");
 	}
 	
 	public Point getPoint() {
@@ -56,6 +26,6 @@ public class Food {
 	}
 
 	public void setPoint(Point point) {
-		this.point = point;
+		throw new UnsupportedOperationException("Not yet implemented");
 	}
 }
